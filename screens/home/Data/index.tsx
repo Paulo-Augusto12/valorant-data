9da01b705af5571564a8dbde6077fa93d54be84d
Paulo <paulo-augusto12@outlook.com>
2023-11-02 01:39:43 -0300

    tela de dados

--- screens/home/Data/index.tsx
+++ screens/home/Data/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ImageBackground,
+  Image,
+} from "react-native";
 import { ValorantAgentDataModel } from "../../../api/agents/getAgents";
 
 function Data({ route, navigation }: any) {
@@ -7,20 +14,56 @@ function Data({ route, navigation }: any) {
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text>{agent.name}</Text>
+      <View
+        style={[
+          styles.agentImagesContainer,
+          { backgroundColor: `#${agent.backgroundColor}` },
+        ]}
+      >
+        <ImageBackground
+          source={{ uri: agent.backgroundImage }}
+          style={styles.backgroundImage}
+        >
+          <Image source={{ uri: agent.image }} style={styles.agentImage} />
+        </ImageBackground>
+        <Text style={styles.agentName}>{agent.name}</Text>
+        <Text style={styles.agentRole}>{agent.role}</Text>
       </View>
     </View>
   );
 }
 
+const height = Dimensions.get("screen").height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#223843",
-    paddingHorizontal: 20,
+    flexDirection: "column",
+  },
+  agentImagesContainer: {
+    height: (60 / 100) * height,
+    width: "100%",
     alignItems: "center",
-    justifyContent: "center",
+    borderRadius: 32,
+    padding: 32,
+  },
+  backgroundImage: {
+    width: "100%",
+  },
+  agentImage: {
+    width: "100%",
+    height: "80%",
+  },
+  agentName: {
+    color: "#FFFF",
+    fontSize: 44,
+    fontWeight: "bold",
+  },
+  agentRole: {
+    color: "#FFFF",
+    fontSize: 24,
+    fontWeight: "600",
   },
 });
 
