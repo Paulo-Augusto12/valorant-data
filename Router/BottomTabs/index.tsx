f1c157a988c5063c220ac6c7dc98e53ce632c7b0
Paulo <paulo-augusto12@outlook.com>
2023-11-02 00:36:34 -0300

    adicionadas telas de mapas e armas

--- Router/BottomTabs/index.tsx
+++ Router/BottomTabs/index.tsx
@@ -1,6 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import home from "../../screens/home";
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import maps from "../../screens/maps";
+import guns from "../../screens/guns";
 
 export function BottomTabs() {
   const Tabs = createBottomTabNavigator();
@@ -22,6 +24,36 @@ export function BottomTabs() {
             return <MaterialIcons name="people" color={color} size={size} />;
           },
           tabBarLabel: "Agentes",
+          tabBarLabelStyle: {
+            fontSize: 15,
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="maps"
+        component={maps}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            size = 30;
+            return <MaterialIcons name="map" color={color} size={size} />;
+          },
+          tabBarLabel: "Mapas",
+          tabBarLabelStyle: {
+            fontSize: 15,
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="guns"
+        component={guns}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            size = 30;
+            return (
+              <MaterialCommunityIcons name="target" color={color} size={size} />
+            );
+          },
+          tabBarLabel: "Armas",
           tabBarLabelStyle: {
             fontSize: 15,
           },
